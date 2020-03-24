9ca562bd1b303c6d40a7b7bfc73cfdbc64406947
Osman Branjerdporn <48505852+OsmanBran@users.noreply.github.com>
2020-03-24 17:05:42 +1000

    Tabs

---
+++ components/Tabs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { ButtonGroup } from 'react-native';
+
+export default class Tabs extends Component {
+    constructor() {
+        super();
+        this.state = {
+           selectedIndex: 1,
+        }
+        this.updateIndex = this.updateIndex.bind(this)
+    }
+
+    updateIndex (selectedIndex) {
+        this.setState({selectedIndex})
+    }
+
+    render() {
+        const buttons = ['Recurring weekly', 'Custom']
+        const { selectedIndex } = this.state
+
+        return(
+            <ButtonGroup
+                onPress={this.updateIndex}
+                selectedIndex={ selectedIndex }
+                buttons={buttons}
+                containerStyle={{height: 100}}
+            />
+      )
+  }
+}
